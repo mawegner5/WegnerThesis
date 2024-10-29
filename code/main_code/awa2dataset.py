022c0c9e6ff1defbc5f7939c9eab9e02e52b125a
root <root@e-bgbfbobm-crnnq-0.e-bgbfbobm-crnnq.ace-hub.svc.cluster.local>
2024-10-29 00:24:24 +0000

    update with new pod running two mulitlabel cnns

---
+++ code/main_code/awa2dataset.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+class AWA2Dataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = sorted(os.listdir(root_dir))
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.image_paths = []
+        self.labels = []
+        for cls_name in self.classes:
+            cls_dir = os.path.join(root_dir, cls_name)
+            for img_name in os.listdir(cls_dir):
+                img_path = os.path.join(cls_dir, img_name)
+                self.image_paths.append(img_path)
+                self.labels.append(self.class_to_idx[cls_name])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert('RGB')
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
